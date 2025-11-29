987de86be616d00c7f57f9db693476c16d8f64ca
Joel <estebanjoelpaez@gmail.com>
2025-11-29 11:21:08 -0300

    Ajuste a Get en listar tareas

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('TareasController (e2e)', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +15,27 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/crear-tareas (POST) debería crear una tarea', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/crear-tareas')
+      .send({ titulo: 'Aprender Nest', descripcion: 'Repasar módulos' })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('mensaje', 'Tarea registrada');
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('/listar-tareas (POST) debería listar tareas', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/listar-tareas')
+      .send({})
+      .expect(201);
+
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body[0]).toHaveProperty('titulo');
   });
 });
